8478d8d8a344d7c271cb843053c20b4d70e6855a
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-08-02 15:17:44 +0700

    feat: Add font family - Montserrat

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="relative flex flex-col h-screen justify-between bg-[url('../public/bgmovie.jpg')] bg-no-repeat bg-cover">
+    <div className="relative font-montserrat flex flex-col h-screen justify-between bg-[url('../public/bgmovie.jpg')] bg-no-repeat bg-cover">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-80"></div>
 
--- src/components/BookTickets.jsx
+++ src/components/BookTickets.jsx
@@ -2,7 +2,7 @@ const BookTickets = () => {
   return (
     <div className="container mx-auto my-6">
       {/* Title */}
-      <h1 className="">BOOK MOVIE TICKETS</h1>
+      <h1 className="text-yellow-500 font-bold">BOOK MOVIE TICKETS</h1>
       {/* Book Tickets Screen */}
       {/* List of selected seats */}
     </div>
