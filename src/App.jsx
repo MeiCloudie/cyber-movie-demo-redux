3870155897661d3cea2e97650ed0465da613258b
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-08-02 15:21:25 +0700

    fix: Fix layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="relative font-montserrat flex flex-col h-screen justify-between bg-[url('../public/bgmovie.jpg')] bg-no-repeat bg-cover">
+    <div className="relative font-montserrat bg-[url('../public/bgmovie.jpg')] bg-no-repeat bg-cover">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-80"></div>
 
       {/* Content */}
-      <BookTickets />
-      <div className="relative z-10">
+      <div className="relative z-10 flex flex-col h-screen justify-between">
+        <BookTickets />
         <Footer />
       </div>
     </div>
